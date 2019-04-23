e15bd656bd167cbda6e3f9b3c03df1f6aa995cb0
jiajunwan <jiajunwan@umail.ucsb.edu>
2019-04-23 02:53:13 -0700

    Basic structure

--- pthread.c
+++ pthread.c
@@ -8,22 +8,39 @@
 #include <sys/wait.h>
 #include <setjmp.h>
 
-#define MAX_THREADS = 128
+#define MAX_THREADS 128
 
 jmp_buf buf;
 
 struct ThreadControlBlock
 {
-    int Registers;
+    jmp_buf Registers;
     int *ESP;
-    int status;
+    /* 0 for running; 1 for ready to run; 2 for exited */
+    int Status;
+};
 
+struct TCBPool
+{
+    struct ThreadControlBlock ThreadPool[MAX_THREADS];
 };
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg)
 {
-
-    return 0;
+    static int Initialized = 0;
+    pthread_t ThreadID;
+    /* If not initialized, initialize thread pool */
+    if (!Initialized)
+    {
+        /* Initialize the main thread pool */
+        
+    }
+    else
+    {
+        
+    }
+    
+    return 
 }
 
 void pthread_exit(void *value_ptr)
